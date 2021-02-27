f0db1db7be2870162183f856e6b6d58c5752a2ec
Tom Andrews <git@tomandrews.co.uk>
2021-02-27 09:09:30 +0000

    Stop waiting

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,7 @@
 import { render } from "@testing-library/react";
 import App from "./App";
 
-const wait = time => new Promise(res => setTimeout(res, time));
-
 test("renders the events", async () => {
   const { asFragment } = render(<App />);
-  await wait(30000);
   expect(asFragment()).toMatchSnapshot();
-}, 60000);
+});
