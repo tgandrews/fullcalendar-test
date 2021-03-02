9fa234eb43d36cddc02a553771fec969978b357d
Tom Andrews <git@tomandrews.co.uk>
2021-03-02 09:57:41 +0000

    Add await as requested

--- src/Calendar.test.js
+++ src/Calendar.test.js
@@ -1,10 +1,13 @@
 import { render } from "@testing-library/react";
 import Calendar from "./Calendar";
 
+const wait = timeout => new Promise(res => setTimeout(res, timeout));
+
 test("renders the events", async () => {
   const { asFragment, container } = render(<Calendar />);
+  await wait(5000);
   expect(asFragment()).toMatchSnapshot();
   expect(
     [...container.querySelectorAll(".fc-timegrid-event")].length,
   ).not.toEqual(0);
-});
+}, 30000);
